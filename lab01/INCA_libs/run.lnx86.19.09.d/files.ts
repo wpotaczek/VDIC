0de45e7d231e9af57e042a0bf2e45c833e8f351c
Potaczek Weronika <wpotaczek@cadence212.(none)>
2020-10-29 23:05:10 +0100

    Fix bugs

--- lab01/INCA_libs/run.lnx86.19.09.d/files.ts
+++ lab01/INCA_libs/run.lnx86.19.09.d/files.ts
@@ -1,3 +1,3 @@
 1547667889 /home/student/wpotaczek/cds.lib
 1602615747 /home/student/wpotaczek/VDIC/lab01/mtm_Alu.vp
-1604008665 /home/student/wpotaczek/VDIC/lab01/alu_tb.sv
+1604008742 /home/student/wpotaczek/VDIC/lab01/alu_tb.sv
