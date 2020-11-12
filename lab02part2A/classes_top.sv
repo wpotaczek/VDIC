a20b495100fe67a8a93a93cbd9c8a651ffdd897a
Potaczek Weronika <wpotaczek@cadence212.(none)>
2020-11-12 23:23:50 +0100

    Final version

--- lab02part2A/classes_top.sv
+++ lab02part2A/classes_top.sv
@@ -16,15 +16,15 @@ class rectangle extends shape;
 	
 	function new(real w, real h);
 		super.new(w,h);
-	endfunction
+	endfunction : new
 	
 	function real get_area();
 		return (width*height);
-	endfunction
+	endfunction : get_area
 	
 	function void print();
 		$display("Rectangle w=%g h=%g area=%g", width, height, get_area());
-	endfunction
+	endfunction : print
 	
 endclass
 
@@ -32,15 +32,15 @@ class square extends shape;
 	
 	function new(real w, real h);
 		super.new(w,w);
-	endfunction
+	endfunction :new
 	
 	function real get_area();
 		return (width*width);
-	endfunction
+	endfunction : get_area
 	
 	function void print();
-		$display("Square w=%g h=%g area=%g", width, height, get_area());
-	endfunction
+		$display("Square w=%g area=%g", width, get_area());
+	endfunction : print
 	
 endclass
 
@@ -48,15 +48,15 @@ class triangle extends shape;
 	
 	function new(real w, real h);
 		super.new(w,h);
-	endfunction
+	endfunction : new
 	
 	function real get_area();
 		return (0.5*width*height);
-	endfunction
+	endfunction : get_area
 	
 	function void print();
 		$display("Triangle w=%g h=%g area=%g", width, height, get_area());
-	endfunction
+	endfunction : print
 	
 endclass
 
@@ -84,7 +84,7 @@ class shape_factory;
 				$fatal(1, {"No such shape: ", shape_type});
 		endcase
 	endfunction
-endclass :shape_factory
+endclass : shape_factory
 
 class shape_reporter #(type T = shape);
 	
@@ -92,66 +92,66 @@ class shape_reporter #(type T = shape);
 	
 	static function collect_shapes(T l);
 		shape_storage.push_back(l);
-	endfunction
+	endfunction : collect_shapes
 	
 	static function void report_shapes();
 		real area_sum = 0;
 		foreach(shape_storage[i])
 		begin
 			shape_storage[i].print();
-		area_sum += shape_storage[i].get_area();
+			area_sum += shape_storage[i].get_area();
 		end
 		$display("Total area: %g\n", area_sum);
-	endfunction
+	endfunction : report_shapes
 endclass : shape_reporter		
 			
 			
 module top;
 	 
-	 initial begin
-		 shape shape_h;
-		 rectangle rectangle_h;
-		 square square_h;
-		 triangle triangle_h;
+	initial begin
+		shape shape_h;
+		rectangle rectangle_h;
+		square square_h;
+		triangle triangle_h;
+		
+		bit cast_ok;
 		 
-		 bit cast_ok;
+		int file_h;
+		real width;
+		real height;
+		string shape_name;
 		 
-		 int file_h;
-		 real width;
-		 real height;
-		 string shape_name;
+		file_h = $fopen("lab02part2A_shapes.txt", "r");
 		 
-		 file_h = $fopen("lab02part2A_shapes.txt", "r");
-		 
-		 while($fscanf(file_h,"%s %g %g", shape_name, width, height) == 3) begin
+		while($fscanf(file_h,"%s %g %g", shape_name, width, height) == 3) begin
 			 
-			 shape_h = shape_factory::make_shape(shape_name, width, height);
+			shape_h = shape_factory::make_shape(shape_name, width, height);
 			 
-			 case(shape_name)
-				 "rectangle" : begin
-					 cast_ok = $cast(rectangle_h, shape_h);
-					 if(!cast_ok)
-						 $fatal(1, "Failed to cast shape_h to rectangle_h");
-					 shape_reporter#(rectangle)::collect_shapes(rectangle_h);
-				 end
-				 "square" : begin
-					 cast_ok = $cast(square_h, shape_h);
-					 if(!cast_ok)
-						 $fatal(1, "Failed to cast shape_h to square_h");
-					 shape_reporter#(square)::collect_shapes(square_h);
-				 end
-				 "triangle" : begin
-					 cast_ok = $cast(triangle_h, shape_h);
-					 if(!cast_ok)
-						 $fatal(1, "Failed to cast shape_h to rectangle_h");
-					 shape_reporter#(triangle)::collect_shapes(triangle_h);
-				 end
-				 default : 
-				 	$fatal (1, {"No such shape: ", shape_name});
-			 endcase
-		 end
-		 shape_reporter#(rectangle)::report_shapes();
-		 shape_reporter#(square)::report_shapes();
-		 shape_reporter#(triangle)::report_shapes();
-	 end			 
+			case(shape_name)
+				"rectangle" : begin
+				cast_ok = $cast(rectangle_h, shape_h);
+				if(!cast_ok)
+					$fatal(1, "Failed to cast shape_h to rectangle_h");
+				shape_reporter#(rectangle)::collect_shapes(rectangle_h);
+			end
+			"square" : begin
+				cast_ok = $cast(square_h, shape_h);
+				if(!cast_ok)
+					$fatal(1, "Failed to cast shape_h to square_h");
+				shape_reporter#(square)::collect_shapes(square_h);
+			end
+			"triangle" : begin
+				cast_ok = $cast(triangle_h, shape_h);
+				if(!cast_ok)
+					$fatal(1, "Failed to cast shape_h to rectangle_h");
+				shape_reporter#(triangle)::collect_shapes(triangle_h);
+			end
+			default : 
+				$fatal (1, {"No such shape: ", shape_name});
+			endcase
+		end
+		shape_reporter#(rectangle)::report_shapes();
+		shape_reporter#(square)::report_shapes();
+		shape_reporter#(triangle)::report_shapes();
+	end			 
 endmodule
