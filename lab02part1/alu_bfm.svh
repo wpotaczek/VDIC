55f828e9104c37035770201d4b077d418aa9ff7a
Potaczek Weronika <wpotaczek@cadence212.(none)>
2020-11-12 16:37:39 +0100

    Add task send_op

--- lab02part1/alu_bfm.svh
+++ lab02part1/alu_bfm.svh
@@ -180,7 +180,7 @@ import alu_pkg::*;
 	end
 	endtask
 	
-	task automatic decode_sin(ref [7:0] q_A[$], ref [7:0] q_B[$], ref [7:0] q_CTL[$], output [31:0] A_data, output [31:0] B_data, output [2:0] op_data);
+	task automatic decode_sin(ref [7:0] q_A[$], ref [7:0] q_B[$], ref [7:0] q_CTL[$], output [31:0] A_data, output [31:0] B_data, operation_t op_data);
 	begin
 				
 		if((q_A.size() < 4) | (q_B.size() < 4) | (q_CTL.size() > 1)) begin
@@ -269,6 +269,56 @@ import alu_pkg::*;
       @(negedge clk);
       @(negedge clk);
       rst_n = 1'b1;
-	  endtask : reset_alu
+	endtask : reset_alu
+	
+	task send_op(input bit [31:0] A, input bit [31:0] B, operation_t op_set);
+		@(negedge bfm.clk);
+			case (op_set) // handle the start signal        	
+         	er_data_op: begin : case_er_data_op 
+	         	/*if(err_data_rand) begin
+			     		bfm.send_byte(CMD_TYPE, {1'b0, add_op, bfm.crc4_generate({B,A,1'b1,add_op},4'h0)});
+        				bfm.send_byte(DATA_TYPE, B[23:16]);
+        				bfm.send_byte(DATA_TYPE, B[15:8]);
+        				bfm.send_byte(DATA_TYPE, B[7:0]);
+        
+        				bfm.send_byte(DATA_TYPE, A[31:24]);
+        				bfm.send_byte(DATA_TYPE, A[23:16]);
+        				bfm.send_byte(DATA_TYPE, A[15:8]);
+			         bfm.send_byte(DATA_TYPE, A[7:0]);
+	         	
+	         		bfm.send_byte(CMD_TYPE, {1'b0, add_op, bfm.crc4_generate({B,A,1'b1,add_op},4'h0)});
+	         		bfm.send_byte(1'b1,{8'b11111111});
+			         	
+			         err_data_rand = 1'b0;
+		        	end
+	         	else begin*/
+			     		bfm.send_byte(DATA_TYPE, B[31:24]);
+        				bfm.send_byte(DATA_TYPE, B[23:16]);
+        				bfm.send_byte(DATA_TYPE, B[15:8]);
+        				bfm.send_byte(DATA_TYPE, B[7:0]);
+        
+     					bfm.send_byte(DATA_TYPE, A[31:24]);
+        				bfm.send_byte(DATA_TYPE, A[23:16]);
+        				bfm.send_byte(DATA_TYPE, A[15:8]);
+	         	
+	         		bfm.send_byte(CMD_TYPE, {1'b0, add_op, bfm.crc4_generate({B,A,1'b1,add_op},4'h0)});
+	         		bfm.send_byte(1'b1,{8'b11111111});
+			         	
+			         //err_data_rand = 1'b1;
+		         //end
+	         end         	
+         	er_crc_op: begin : case_er_crc_op
+	         	//crc_error = (bfm.crc4_generate({B,A,1'b1,op},4'h0) + 1'b1);
+        			bfm.send_calculation_data(B, A, add_op, (bfm.crc4_generate({B,A,1'b1,op_set},4'h0) + 1'b1));
+         	end
+         	er_op_op: begin : case_er_op_op
+	         	bfm.send_calculation_data(B, A, op_set, bfm.crc4_generate({B,A,1'b1,op_set},4'h0));
+         	end
+           default: begin
+	           bfm.send_calculation_data(B, A, op_set, bfm.crc4_generate({B,A,1'b1,op_set},4'h0));
+           end
+         endcase
+	endtask : send_op
+	
 
 endinterface : alu_bfm
